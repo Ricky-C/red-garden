d3b272e96abce19c5dae21e3e25f5cc2cd9cf8cb
Ricky-C <cabaethernet@gmail.com>
2018-04-03 03:35:05 -0400

    |add|Ingredient Animation

--- src/js/file1.js
+++ src/js/file1.js
@@ -9,3 +9,27 @@ function showPage() {
   document.getElementById("page").style.display = "block";
 }
 
+function pomLearn() {
+	var pom = document.querySelector(".pom-learn");
+	var fill1 = document.querySelector(".pom")
+	pom.addEventListener("click", function () {
+		fill1.classList.add("pom-info");
+		
+	});
+}
+
+function tomLearn() {
+	var tom = document.querySelector(".tom-learn");
+	var fill2 = document.querySelector(".tom")
+	tom.addEventListener("click", function () {
+		fill2.classList.add("tom-info");
+	});
+}
+
+function cranLearn() {
+	var cran = document.querySelector(".cran-learn");
+	var fill3 = document.querySelector(".cran")
+	cran.addEventListener("click", function () {
+		fill3.classList.add("cran-info");
+	});
+}

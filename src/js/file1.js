6a9a539142d8de398159ed853c10266fc7c385cd
Ricky-C <cabaethernet@gmail.com>
2018-05-10 20:25:31 -0400

    |add|About/ Contact/ Cater Pages

--- src/js/file1.js
+++ src/js/file1.js
@@ -14,6 +14,8 @@ function pomLearn() {
 	var fill1 = document.querySelector(".pom")
 	pom.addEventListener("click", function () {
 		fill1.classList.add("pom-info");
+		fill1.innerHTML = "Pomegranates help our body in many ways from curing stomach problems to improving erectile dysfunctions and more.";
+		fill1.style.cssText = "padding-right: 5px; padding-top: 10px; max-width: 100%;";
 		
 	});
 }
@@ -23,6 +25,8 @@ function tomLearn() {
 	var fill2 = document.querySelector(".tom")
 	tom.addEventListener("click", function () {
 		fill2.classList.add("tom-info");
+		fill2.innerHTML = "The health benefits of tomatoes include eye care, good stomach health, and a reduced blood pressure and relief from diabetes.";
+		fill2.style.cssText = "padding-right: 5px; padding-top: 10px;";
 	});
 }
 
@@ -31,5 +35,8 @@ function cranLearn() {
 	var fill3 = document.querySelector(".cran")
 	cran.addEventListener("click", function () {
 		fill3.classList.add("cran-info");
+		fill3.innerHTML = "The health benefits of cranberries include relief from UTIs, respiratory disorders, kidney stones, cancer, and heart diseases.";
+		fill3.style.cssText = "padding-right: 5px; padding-top: 10px;";
 	});
 }
+
